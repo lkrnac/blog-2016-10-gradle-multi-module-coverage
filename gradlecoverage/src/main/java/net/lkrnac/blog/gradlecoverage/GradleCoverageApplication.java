38f9d73a201e0bcfe064fc177cfaae39282a345c
Lubos Krnac <lubos.krnac@gmail.com>
2016-10-16 18:16:57 +0530

    Initial commit of gradle multi-module project

---
+++ gradlecoverage/src/main/java/net/lkrnac/blog/gradlecoverage/GradleCoverageApplication.java
@@ -0,0 +1,10 @@
+package net.lkrnac.blog.gradlecoverage;
+
+/**
+ * Created by: Lubos Krnac
+ * For: Dotsub LLC.
+ */
+public class GradleCoverageApplication {
+    public static void main(String... args) {
+    }
+}
